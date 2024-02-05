11db4f2ed5f00413c7514fb4d0f929672df4862b
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-02-06 01:23:34 +0900

    optinal 인자를 config으로 이동

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
-"""프로젝트의 공통 변수를 설정합니다."""
-from pathlib import Path
+"""프로젝트에서 사용되는 변수의 기본값을 관리합니다."""
 
-ROOT = Path(__file__).parent
-MODEL_DIRECTORY = Path(ROOT, "model")
-
-# 디렉토리 구성
-MODEL_DIRECTORY.mkdir(parents=True, exist_ok=True)
-
-# 변수 설정
-path = {"sklearn_model": Path(MODEL_DIRECTORY, "iris_classification_model.joblib")}
+mlflow = {
+    "tracking_uri": "http://localhost:5000",
+    "plot_name": "feature_importances.png",
+    "artifact_path": "model",
+    "model_format": "ubj",
+    "n_trials": 100,
+    "split_ratio": 0.25,
+}
